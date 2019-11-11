4915bd6f8a2790812ea9a630e3fbff3360b044e7
JohnMags99 <48322150+JohnMags99@users.noreply.github.com>
2019-11-11 07:02:46 +0000

    Add files via upload

--- src/AdvancePurchase.java
+++ src/AdvancePurchase.java
@@ -1,9 +1,33 @@
 import java.util.Date;
 
 public class AdvancePurchase extends Reservation {
+
+    public AdvancePurchase(String firstName, String lastName, int numberOfNights, int day, int month, int year) {
+        Date reservationDate = new Date();
+        this.day = day;
+        this.month = month;
+        this.year = year;
+        checkInDate = new Date(year, month, day);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.numberOfNights = numberOfNights;
+        for (int i = 0; i < reservationNumbers.size(); i++) {
+            reservationNumber = (double) (Math.random() * 100000);
+            if (reservationNumbers.contains(reservationNumber)) {
+                reservationNumber = (double) (Math.random() * 100000);
+            }
+            reservationNumbers.add(reservationNumber);
+        }
+        for (int i = 0; i < rooms.size(); i++) {
+            sum += (rooms.getPrice()).get(i);
+        }
+        double numNightstotal = sum * numberOfNights;
+        totalPrice = numberOfNights + this.getDeposit();
+    }
+
     @Override
     public void makeReservation(String firstName, String lastName, int numberOfNights, int day, int month, int year){} {
-        Reservation reservation = new Reservation(firstName, lastName, numberOfNights, day, month, year);
+        Reservation reservation = new AdvancePurchase(firstName, lastName, numberOfNights, day, month, year);
         reservations.add(reservation);
         double discounted = totalPrice * (19/20);
         System.out.println("Congratulations!" + "\n" + "You have booked " + rooms.size() + ", rooms for " + numberOfNights + " nights." + "\n" + "You're checking in on " + checkInDate + ".");
