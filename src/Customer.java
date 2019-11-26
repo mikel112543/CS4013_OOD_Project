13b0c4321566b26711e4e74bf2a652df09af32e2
mikel112543 <michaelthomasdanaher@gmail.com>
2019-11-26 02:19:38 +0000

    Updated Customer
    Signed-off-by: mikel112543 <michaelthomasdanaher@gmail.com>

--- src/Customer.java
+++ src/Customer.java
@@ -1,6 +1,10 @@
+import java.io.FileNotFoundException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Customer {
@@ -22,19 +26,18 @@ public class Customer {
     protected int adultOcc;
     protected int childOcc;
     protected boolean breakfast;
-    protected  int priceOfRoom;
+    protected int priceOfRoom;
     private String hotelType;
+    private Reservation reservation;
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
 
     protected int totalCost;        //8
     protected int deposit;          //9
 
+    public void getUserInput() throws FileNotFoundException {
 
-
-    public void getUserInput(){
-
-        Reservation r = new Reservation();
         ArrayList<Room> rooms = new ArrayList<>();
-
 
         Scanner input = new Scanner(System.in);
         System.out.println("M)ake Reservation   Q)uit   S)how Reservations");
@@ -55,73 +58,82 @@ public class Customer {
             year = input.nextInt();
             System.out.println("Month of Check-In? (1-12)");
             month = input.nextInt();
-            if(month == 4 || month == 6 || month == 9 || month ==11){
+            if (month == 4 || month == 6 || month == 9 || month == 11) {
                 System.out.println("Day of Check-In? (1-30)");
-            }
-            else if(month == 2 && year % 4 == 0){
+            } else if (month == 2 && year % 4 == 0) {
                 System.out.println("Day of Check-In? (1-29");
-            }
-            else if(month == 2 && year % 4 != 0){
+            } else if (month == 2) {
                 System.out.println("Day of Check-In? (1-28)");
-            }
-            else {
+            } else {
                 System.out.println("Day of Check-In? (1-31");
             }
             day = input.nextInt();
-            ReservationDate checkIn = new ReservationDate(day, month, year);
-
-            if(month == 1 || month == 2){
-                month = (month == 1) ? 13:14;
+            if (month == 1 || month == 2) {
+                month = (month == 1) ? 13 : 14;
                 year--;
             }
             int dayOfWeek = (day + (26 * (month + 1)) / 10 + (year % 100)
                     + (year % 100) / 4 + (year / 100) / 4 + 5 * (year / 100)) % 7;
+            LocalDate checkIn =  LocalDate.of(year, month, day);
 
-            switch(dayOfWeek){
-                case 0: checkInDay = ("Saturday"); break;
-                case 1: checkInDay = ("Sunday"); break;
-                case 2: checkInDay = ("Monday"); break;
-                case 3: checkInDay = ("Tuesday"); break;
-                case 4: checkInDay = ("Wednesday"); break;
-                case 5: checkInDay = ("Thursday"); break;
-                case 6: checkInDay = ("Friday"); break;
+
+            switch (dayOfWeek) {
+                case 0:
+                    checkInDay = ("Saturday");
+                    break;
+                case 1:
+                    checkInDay = ("Sunday");
+                    break;
+                case 2:
+                    checkInDay = ("Monday");
+                    break;
+                case 3:
+                    checkInDay = ("Tuesday");
+                    break;
+                case 4:
+                    checkInDay = ("Wednesday");
+                    break;
+                case 5:
+                    checkInDay = ("Thursday");
+                    break;
+                case 6:
+                    checkInDay = ("Friday");
+                    break;
             }
-
-
 
             System.out.println("How many rooms would you like to book?");
             numberOfRooms = input.nextInt();
 
-
             System.out.println("Select Star Hotel (5, 4, 3)");
             hotel = input.nextInt();
-            if(hotel == 5) {
+            if (hotel == 5) {
                 hotelType = "Five Star";
-            }else if(hotel == 4) {
+            } else if (hotel == 4) {
                 hotelType = "Four Star";
-            }else if(hotel == 3) {
+            } else if (hotel == 3) {
                 hotelType = "Three Star";
             }
 
-            String[] fiveStarHotel = new String []{"Deluxe Double" , "Deluxe Twin" , "Deluxe Single" , "Deluxe Family"};
-            String[] fourStarHotel = new String[]{"Executive Double" , "Executive Twin" , "Executive Single"};
-            String[] threeStarHotel = new String[]{"Classic Double" , "Classic Twin" , "Classic Single"};
+            String[] fiveStarHotel = new String[]{"Deluxe Double", "Deluxe Twin", "Deluxe Single", "Deluxe Family"};
+            String[] fourStarHotel = new String[]{"Executive Double", "Executive Twin", "Executive Single"};
+            String[] threeStarHotel = new String[]{"Classic Double", "Classic Twin", "Classic Single"};
 
             int i = 1;
             int typeOfRoom;
             while (i <= numberOfRooms) {
-                System.out.println("Do you want breakfast for room " + i + "(Y/N)");
-                String decision = input.next();
-                breakfast = decision.equalsIgnoreCase("y");
+
 
                 System.out.println("Select room type for room " + i);
                 switch (hotel) {
                     case 5:
                         for (int j = 0; j < fiveStarHotel.length; j++) {
-                            System.out.print((j + 1)+ ")");
+                            System.out.print((j + 1) + ")");
                             System.out.println(fiveStarHotel[j]);
                         }
                         typeOfRoom = input.nextInt();
+                        System.out.println("Do you want breakfast for room " + i + "(Y/N)");
+                        String decision = input.next();
+                        breakfast = decision.equalsIgnoreCase("y");
 
 
                         switch (typeOfRoom) {
@@ -143,7 +155,7 @@ public class Customer {
 
                     case 4:
                         for (int j = 0; j < fourStarHotel.length; j++) {
-                            System.out.print((j + 1)+ ")");
+                            System.out.print((j + 1) + ")");
                             System.out.println(fourStarHotel[j]);
                         }
                         typeOfRoom = input.nextInt();
@@ -152,20 +164,18 @@ public class Customer {
                         break;
                     default:
                         for (int j = 0; j < threeStarHotel.length; j++) {
-                            System.out.print((j + 1)+ ")");
+                            System.out.print((j + 1) + ")");
                             System.out.println(threeStarHotel[j]);
                         }
                         typeOfRoom = input.nextInt();
                         occupancyDoubleSingleTwin(input, typeOfRoom);
                         roomType = threeStarHotel[typeOfRoom - 1];
-
-
                 }
 
                 //checkInDay (eg mon), number of nights (eg 20), then get Cost of each room.
                 //number of nights / 7 = number of weeks stayed.... numberOfWeeks x costOfOneWeek = costForAllWeeks.
                 //number of nights % 7 = leftover days. ifCheckInDay is monday, go monday + leftoverDays.. eg if Monday and 6 leftOver is 2, monday + tuesday.
-                Room room = new Room(hotelType, roomType, adultOcc, childOcc, numberOfNights, breakfast, priceOfRoom, checkIn, checkInDay);
+                Room room = new Room(roomType, adultOcc, childOcc, breakfast, checkIn);
                 rooms.add(room);
                 i++;
                 //method to add the rom into the arraylist of rooms with parameters being "roomType, adultOcc, childOcc, breakfast, cost";
@@ -175,26 +185,23 @@ public class Customer {
             if (reserve.equalsIgnoreCase("y")) {
                 System.out.println("What type of Reservation method will you be using? " + "\n S)Standard Booking" + "\n AP)Advanced Purchase");
                 resType = input.next().toUpperCase();
-                if(resType.equalsIgnoreCase("s")) {
+                if (resType.equalsIgnoreCase("s")) {
                     typeOfPurchase = "Standard Booking";
                 }
-                Reservation reservation = new Reservation(resNumber, firstNameOnRes, lastNameOnRes, rooms, numberOfNights, checkIn, checkInDay, typeOfPurchase);
-                reservation.addReservation(reservation);
+                Reservation reservation = new Reservation(resNumber, firstNameOnRes, lastNameOnRes, hotelType, rooms, numberOfNights, checkIn, typeOfPurchase);
+                reservation.makeReservation(reservation);
                 getUserInput();
-            }else{
+            } else {
                 getUserInput();
             }
 
-        }else if(choice.equals("S")) {
-            r.showReservations();
+        } else if (choice.equals("S")) {
+            reservation.showReservations();
             getUserInput();
-        }else{
+        } else {
 
             System.out.println("Thank you for using L4 Hotels");
         }
-
-
-
     }
 
     public void occupancyDoubleSingleTwin(Scanner input, int typeOfRoom) {
@@ -219,5 +226,4 @@ public class Customer {
                 break;
         }
     }
-
 }
